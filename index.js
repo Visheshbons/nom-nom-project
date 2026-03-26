442a6053a093cf3a3dd012432abd7a792f0646ad
Vishesh Kudva <vishesh.kudva@outlook.com>
2026-03-26 18:41:15 +1300

    Added neccecary base files

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+// ---------- IMPORTS ---------- \\
 import express from "express";
 import chalk from "chalk";
 
+// ---------- CONSTANTS ---------- \\
 const app = express();
 const port = 3000;
 
+// ---------- ROUTES ---------- \\
+app.get("/", (req, res) => {
+  res.send("Welcome to the nom-nom project!");
+});
+
+// ---------- SERVER INIT ---------- \\
 app.listen(port, () => {
   console.log(`Server running on port ${chalk.green(port)}.`);
 });
