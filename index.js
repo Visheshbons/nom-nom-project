9ea324a7a1e84cbb9b55f5f5a4e98daf5b372748
Vishesh Kudva <vishesh.kudva@outlook.com>
2026-04-14 20:30:34 +1200

    Added copyright

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // ---------- ROUTES ---------- \\
-app.get("/", (req, res) => {
-  res.render("index");
+app.use((req, res) => {
+  res.render("index", { page: req.path });
 });
 
 // ---------- SERVER INIT ---------- \\
